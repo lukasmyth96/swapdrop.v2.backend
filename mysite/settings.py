59ef8ca8533bd1b664f499bc2bc16f3a621d780c
lukasmyth96 <luka@nate.tech>
2020-07-18 17:03:58 +0100

    git ignoring vscode stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,10 +75,8 @@ JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER': 'mysite.utils.my_jwt_response_handler'
 }
 
-#JWT_EXPIRATION_DELTA = 3600
-#JWT_REFRESH_EXPIRATION_DELTA = 3600
+JWT_EXPIRATION_DELTA = datetime.timedelta(seconds=1000000)
 
-JWT_VERIFY_EXPIRATION = False
 
 ROOT_URLCONF = 'mysite.urls'
 
