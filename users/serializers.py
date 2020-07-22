8a9be4cd3b988e98427fd9ea83179d78bd2bdfd7
lukasmyth96 <luka@nate.tech>
2020-07-22 09:10:57 +0100

    added fields to User serializer

--- users/serializers.py
+++ users/serializers.py
@@ -33,4 +33,4 @@ class UserSerializerWithToken(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('token', 'username', 'password')
+        fields = ('token', 'email', 'username', 'first_name', 'last_name', 'password')
