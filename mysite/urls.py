36a8ac59fa95466ea3d9513c95ca35dbb64bd2da
lukasmyth96 <luka@nate.tech>
2020-07-26 12:52:17 +0100

    fixing static serving

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_jwt.views import obtain_jwt_token
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     path('users/', include('users.urls')),
     path('products/', include('products.urls'))
-
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
