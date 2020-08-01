5bde821bbebeba84f4487739243d0f236f261408
David-Ovidiu <naeovidiu@yahoo.com>
2020-08-01 14:44:18 +0300

    More changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('products.urls')),
+    path('offers/', include('offers.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- offers/views.py
+++ offers/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+from products.models import Product
+
+
+class MakeOffer(APIView):
+
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request, *args, **kwargs):
+
+        return Response({"test": "test"}, 200)
