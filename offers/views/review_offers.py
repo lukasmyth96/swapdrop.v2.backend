8f5e745b42ead3a670769130bb2f468a8807a0fd
lukasmyth96 <luka@nate.tech>
2020-08-09 14:31:53 +0100

    refactor

--- offers/views/review_offers.py
+++ offers/views/review_offers.py
@@ -8,14 +8,27 @@ from products.models import Product
 from products.model_enums import ProductStatus
 
 
+class ProductNotLive(Exception):
+    pass
+
+
 class ReviewOffersView(APIView):
 
     def get(self, request, product_id):
-        product = Product.objects.get(id=product_id)
-        if product.is_owned_by(self.request.user):   
+        try:
+            product = Product.objects.get(id=product_id)
             offered_products = product.pending_offers.all().filter(status=ProductStatus.LIVE)
             serialized_offers = [ProductSerializer(instance=offered_product).data for offered_product in offered_products]
             response = Response(data=serialized_offers, status=HTTP_200_OK)
-        else:
+        except (PermissionError, ProductNotLive):
             response = HttpResponseForbidden("You do not have permission to view the offers")
         return response
+
+    @staticmethod
+    def assert_product_is_live(product: Product):
+        if not product.is_live:
+            raise ProductNotLive()
+
+    def validate_permissions(self, product: Product):
+        if not product.is_owned_by(self.request.user):
+            raise PermissionError()
