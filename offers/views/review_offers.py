51d2d86da3dbcfeb9bcdde243cec9160db88eb12
lukasmyth96 <luka@nate.tech>
2020-08-15 15:55:56 +0100

    add request context to review offer product serializer

--- offers/views/review_offers.py
+++ offers/views/review_offers.py
@@ -22,12 +22,15 @@ class ReviewOffersView(APIView):
             self.validate_permissions(product=product)
 
             offered_products = product.pending_offers.all().filter(status=ProductStatus.LIVE)
-            serialized_offers = [ProductSerializer(instance=offered_product).data for offered_product in offered_products]
+            serialized_offers = [ProductSerializer(instance=offered_product,
+                                                   context={"request": request}).data 
+                                                   for offered_product in offered_products]
             response = Response(data=serialized_offers, status=HTTP_200_OK)
         except ValidationError as exc:
             response = HttpResponseBadRequest(str(exc.error_list[0]))
         except (PermissionError, ProductNotLive):
-            response = HttpResponseForbidden("You do not have permission to view offers on this product")
+            response = HttpResponseForbidden(
+                "You do not have permission to view offers on this product")
         except Product.DoesNotExist:
             response = HttpResponseNotFound("Product does not exist")
         return response
